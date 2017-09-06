d88129f55e73e566a381e82fa41bcf765384d10b
derekcfarmer <derek.farmer@gmail.com>
2017-09-05 18:02:52 -0700

    adds styling to work page, adjusts margins

--- src/component/work/index.js
+++ src/component/work/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {connect} from 'react-redux';
 import * as util from '../../lib/util.js';
 import * as route from '../../actions/route.js';
+import './work.scss';
 
 import Navbar from '../navbar';
 import Navbutton from '../navbutton';
@@ -20,16 +21,17 @@ class Work extends React.Component {
   render() {
     return (
       <section id='work-page'>
-        <h2> Podcast Host & Consultant </h2>
+        <div id='work-page-buffer-top'></div>
+        <h1> Podcast Host & Consultant </h1>
         <hr/>
         <p> Veggies es bonus vobis, proinde vos postulo essum magis kohlrabi welsh onion daikon amaranth tatsoi tomatillo melon azuki bean garlic. </p>
-        <h2> Freelance Writer </h2>
+        <h1> Freelance Writer </h1>
         <hr/>
         <p> Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato. Dandelion cucumber earthnut pea peanut soko zucchini. </p>
-        <h2> Radio Producer </h2>
+        <h1> Radio Producer </h1>
         <hr/>
         <p> Turnip greens yarrow ricebean rutabaga endive cauliflower sea lettuce kohlrabi amaranth water spinach avocado daikon napa cabbage asparagus winter purslane kale. </p>
-        <h2> Host & Moderator </h2>
+        <h1> Host & Moderator </h1>
         <hr/>
         <p> Celery potato scallion desert raisin horseradish spinach carrot soko.  </p>
         <Navbutton
